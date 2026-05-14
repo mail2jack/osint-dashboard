30eaff6069bebf6db013e6d0c9441cdfc7579979
gast <gast@MacBook-Air-van-Ivan.local>
2026-05-14 13:40:56 +0200

    Sync version.py with VERSION file (3.4.2)

--- version.py
+++ version.py
@@ -18,7 +18,7 @@ Git integration (optional):
 
 from datetime import datetime
 
-VERSION = (3, 4, 1)
+VERSION = (3, 4, 2)
 VERSION_STRING = ".".join(map(str, VERSION))
 VERSION_NAME = "Iveras OSINT"
 
