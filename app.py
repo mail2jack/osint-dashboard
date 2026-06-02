80b3df18a92aba93c5955dd19e737f2460974fc9
gast <gast@MacBook-Air-van-Ivan.local>
2026-06-03 01:13:28 +0200

    chore: set debug=False for production

--- app.py
+++ app.py
@@ -492,4 +492,4 @@ def add_security_headers(response):
 # =============================================================================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
